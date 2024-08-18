b1413fd27e6b2c0aa56aabf58ae1e67a7bd36424
Drew DeCarme <drewdecarme@gmail.com>
2024-08-18 07:22:54 -0400

    updates

--- packages/docs/app/components/LayoutFooter.tsx
+++ packages/docs/app/components/LayoutFooter.tsx
@@ -7,9 +7,14 @@ const footerStyle = css`
   color: white;
   grid-area: footer;
 
-  display: grid;
-  grid-template-columns: minmax(auto, 2fr) repeat(3, 1fr);
-  gap: 3rem;
+  & > div {
+    max-width: 1280px;
+    margin: 0 auto;
+
+    display: grid;
+    grid-template-columns: minmax(auto, 2fr) repeat(3, 1fr);
+    gap: 3rem;
+  }
 
   .title {
     font-size: 14px;
@@ -70,48 +75,50 @@ export function LayoutFooter() {
   return (
     <footer className={footerStyle}>
       <div>
-        <div className="title">React Cartography</div>
-        <p>
-          Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce
-          dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut
-          fermentum massa justo sit amet risus. Etiam porta sem malesuada magna
-          mollis euismod. Duis mollis, est non commodo luctus, nisi erat
-          porttitor ligula, eget lacinia odio sem nec elit.
-        </p>
-        <p>
-          Designed and developed by{" "}
-          <a href="https://github.com/drewdecarme">Drew DeCarme</a>
-        </p>
-      </div>
-      <div>
-        <div className="title">Quick Links</div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/open-layers">Open Layers</Link>
-          </li>
-        </ul>
-      </div>
-      <div>
-        <div className="title">Resources</div>
-        <ul>
-          <li>
-            <Link to="/">Open Layers Documentation</Link>
-          </li>
-        </ul>
-      </div>
-      <div>
-        <div className="title">Usage</div>
-        <ul>
-          <li>
-            <Link to="/">Privacy Policy</Link>
-          </li>
-          <li>
-            <Link to="/">Terms and Conditions</Link>
-          </li>
-        </ul>
+        <div>
+          <div className="title">React Cartography</div>
+          <p>
+            Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce
+            dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh,
+            ut fermentum massa justo sit amet risus. Etiam porta sem malesuada
+            magna mollis euismod. Duis mollis, est non commodo luctus, nisi erat
+            porttitor ligula, eget lacinia odio sem nec elit.
+          </p>
+          <p>
+            Designed and developed by{" "}
+            <a href="https://github.com/drewdecarme">Drew DeCarme</a>
+          </p>
+        </div>
+        <div>
+          <div className="title">Quick Links</div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/open-layers">Open Layers</Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <div className="title">Resources</div>
+          <ul>
+            <li>
+              <Link to="/">Open Layers Documentation</Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <div className="title">Usage</div>
+          <ul>
+            <li>
+              <Link to="/">Privacy Policy</Link>
+            </li>
+            <li>
+              <Link to="/">Terms and Conditions</Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </footer>
   );
--- packages/docs/app/routes/_index.tsx
+++ packages/docs/app/routes/_index.tsx
@@ -2,6 +2,7 @@ import { json, type MetaFunction } from "@remix-run/cloudflare";
 import { Hero } from "../components/Hero";
 import { codeToHtml } from "shiki";
 import Example from "../examples/open-layers.layer-tile.osm";
+import { PageSection } from "../components/PageSection";
 
 export const meta: MetaFunction = () => {
   return [
@@ -25,6 +26,12 @@ export default function Index() {
   return (
     <>
       <Hero />
+      <PageSection
+        title="If you know hooks, you know maps"
+        subTitle="React Cartography supplies hooks that interface the imperative APIs of modern open source mapping libraries. Simply import the hooks and supply some options to get your maps up and running."
+      >
+        test
+      </PageSection>
     </>
   );
 }
